d1a1a17768c0b94e61768d333352300750b76f33
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2023-11-13 19:32:13 +0200

    setup react-native + typescript + nativewind

---
+++ react native/groovy/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-black">
+      <Text className="text-orange-400">MA DEdDUBLEZ</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
