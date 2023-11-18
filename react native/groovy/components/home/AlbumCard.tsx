424dc8849fc51d504b1ee3eb71494e59b28de89c
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2023-11-18 15:53:57 +0200

    implement album list

--- react native/groovy/components/home/AlbumCard.tsx
+++ react native/groovy/components/home/AlbumCard.tsx
@@ -4,6 +4,7 @@ import { Fontisto } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import CustomTitle from "../general/CustomTitle";
+import CustomText from "../general/CustomText";
 
 interface Props {
   children?: ReactNode;
@@ -11,20 +12,20 @@ interface Props {
 
 export default function AlbumCard({ children }: Props) {
   return (
-    <View>
+    <View className="flex flex-col items-center gap-y-2">
       <LinearGradient
         colors={["#ace474", "#000000"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 0, y: 1.5 }}
-        className="w-24 h-24 rounded-lg"
+        className="w-40 h-40 rounded-2xl items-center justify-center"
       >
-        <Fontisto name="music-note" size={24} color="black" />
-        <MaterialIcons name="music-note" size={24} color="black" />
+        <Fontisto name="music-note" size={72} color="black" />
+        {/* <MaterialIcons name="music-note" size={24} color="black" /> */}
       </LinearGradient>
       {children && (
-        <CustomTitle className="text-metallic uppercase text-lg">
+        <CustomText className="text-metallic uppercase text-lg">
           {children}
-        </CustomTitle>
+        </CustomText>
       )}
     </View>
   );
--- react native/groovy/screens/HomeScreen.tsx
+++ react native/groovy/screens/HomeScreen.tsx
@@ -2,17 +2,91 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import GradientText from "../components/general/GradientText";
 import CustomText from "../components/general/CustomText";
 import AddAlbumButton from "../components/home/AddAlbumButton";
-import React from "react";
+import React, { useState } from "react";
 import AlbumCard from "../components/home/AlbumCard";
+import { Album } from "../models/Album";
+import { View } from "react-native";
 
 export default function HomeScreen() {
+  const [albums, setAlbums] = useState<Album[]>([]);
+  const sampleAlbums: Album[] = [
+    {
+      id: 1,
+      title: "Album 1",
+      artist: "Artist 1",
+      year: 2020,
+      genre: "Hip-Hop",
+      no_songs: 10,
+    },
+    {
+      id: 2,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 3,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 4,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 5,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 6,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 7,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+    {
+      id: 8,
+      title: "Album 2",
+      artist: "Artist 2",
+      year: 2021,
+      genre: "Pop",
+      no_songs: 12,
+    },
+  ];
   return (
     <SafeAreaView className="flex-1 items-center justify-top bg-background">
       <GradientText className="text-4xl text-metallic mt-8">
         Groovy
       </GradientText>
       <AddAlbumButton />
-      <AlbumCard>Miaunescovici</AlbumCard>
+      {/* <FlatList></FlatList> */}
+      <View className="flex flex-row flex-wrap justify-center">
+        {sampleAlbums.map((album: Album) => (
+          <AlbumCard key={album.id}>{album.title}</AlbumCard>
+        ))}
+      </View>
     </SafeAreaView>
   );
 }
